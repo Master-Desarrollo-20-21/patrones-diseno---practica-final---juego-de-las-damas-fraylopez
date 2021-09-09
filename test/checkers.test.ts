285ce62514ef6c07b35df4ac0f9bd3becbf52afb
Victor Moreno <victor.moreno@goin.app>
2021-09-09 17:39:44 +0200

    refactor tests

--- test/checkers.test.ts
+++ test/checkers.test.ts
@@ -4,14 +4,30 @@ import { Color } from "../src/contexts/checkers/domain/Color";
 import { Session } from "./../src/contexts/checkers/domain/Session";
 import { expect } from "chai";
 describe('checkers tests', () => {
-  it('should play a AI game', () => {
-    const session = new Session();
-    session.setNumPlayers(0);
-    const playView = new PlayView();
-    const playController = new PlayController(session);
-    while (!session.isGameOver()) {
-      playView.interact(playController);
-    }
-    expect(session.isWinner(Color.Black) || session.isWinner(Color.White));
+  let session: Session;
+  let playController: PlayController;
+  beforeEach(() => {
+    session = new Session();
+    playController = new PlayController(session);
+
   });
+  describe('Play', () => {
+    it('should play a View AI game', () => {
+      session.setNumPlayers(0);
+      const playView = new PlayView();
+      while (!session.isGameOver()) {
+        playView.interact(playController);
+      }
+      expect(session.isWinner(Color.Black) || session.isWinner(Color.White));
+    });
+    it('should play a Controller AI game', () => {
+      session.setNumPlayers(0);
+      const playController = new PlayController(session);
+      while (!session.isGameOver()) {
+        playController.executeMove();
+      }
+      expect(session.isWinner(Color.Black) || session.isWinner(Color.White));
+    });
+  });
+
 });
