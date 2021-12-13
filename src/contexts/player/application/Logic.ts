dc8b44f30563514fbec02f6f53439bd4aaae063e
Victor Moreno <>
2021-12-13 18:19:55 +0100

    viewModel getter

--- src/contexts/player/application/Logic.ts
+++ src/contexts/player/application/Logic.ts
@@ -6,11 +6,14 @@ import { PlayController } from "./PlayController";
 import { ResumeController } from "./ResumeController";
 import { SaveController } from "./SaveController";
 import { StartController } from "./StartController";
+import { IObserver } from "./viewModels/IObserver";
+import { PlayViewModel } from "./viewModels/PlayViewModel";
 
 export class Logic {
 
   private readonly session: Session;
   private readonly controllers: Map<StateValue, IAcceptorController | null>;
+  private readonly viewModels: IObserver[];
   constructor() {
     this.session = new Session();
     this.controllers = new Map();
@@ -20,9 +23,16 @@ export class Logic {
     this.controllers.set(StateValue.Saving, new SaveController(this.session, sessionRepository));
     this.controllers.set(StateValue.Resume, new ResumeController(this.session));
     this.controllers.set(StateValue.Exit, null);
+
+    this.viewModels = [new PlayViewModel(this.session)];
   }
 
   getController(): IAcceptorController | null {
     return this.controllers.get(this.session.getCurrentState())!;
   }
+  getViewModel<T>(type: Constructor): T & IAcceptorController {
+    return this.viewModels.find(c => c instanceof type)! as any as T & IAcceptorController;
+  }
 }
+
+type Constructor<T = {}> = new (...args: any[]) => T;
