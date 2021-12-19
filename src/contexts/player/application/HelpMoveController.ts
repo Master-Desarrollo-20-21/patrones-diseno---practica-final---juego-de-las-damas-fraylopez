27a7f6fddcb2690a4300a519d6c5888da456e700
Victor Moreno <>
2021-12-19 18:48:44 +0100

    change board state: pass

--- src/contexts/player/application/HelpMoveController.ts
+++ src/contexts/player/application/HelpMoveController.ts
@@ -1,7 +1,18 @@
+import { Color } from "../domain/Color";
+import { Move } from "../domain/Move";
+import { Token } from "../domain/Token";
+import { Coordinate } from "../_shared/Coordinate";
 import { SessionController } from "./SessionController";
 
 export class HelpMoveController extends SessionController {
   executeHelpMove() {
+    this.session.setNumPlayers(1);
+    this.session.executeMove(
+      new Move(
+        new Token(Color.Black),
+        new Coordinate(2, 1),
+        new Coordinate(3, 0))
+    );
   }
 
 }
