3f7a766a22d33dee197daff1664c62dd16c4800f
Victor Moreno <>
2021-12-19 18:50:39 +0100

    change board state: refactor test

--- test/contexts/player/application/HelpMoveController.test.ts
+++ test/contexts/player/application/HelpMoveController.test.ts
@@ -4,16 +4,19 @@ import { Session } from "../../../../src/contexts/player/domain/Session";
 import { BoardDAO } from "../../../../src/contexts/player/infrastructure/dao/BoardDAO";
 
 describe('HelpMoveController', () => {
+  let controller: HelpMoveController;
+  let session: Session;
+  before(() => {
+    session = new Session();
+    controller = new HelpMoveController(session);
+  });
   it('should exist', () => {
-    const controller = new HelpMoveController(new Session());
     expect(controller).not.to.be.undefined;
   });
 
   it('should execute a help move', () => {
-    const session = new Session();
     session.startNewGame();
     const initialBoardState = new BoardDAO(session.getGame().getBoard()).serialize();
-    const controller = new HelpMoveController(session);
     controller.executeHelpMove();
     const newBoardState = new BoardDAO(session.getGame().getBoard()).serialize();
     expect(JSON.stringify(newBoardState)).not.equal(JSON.stringify(initialBoardState));
